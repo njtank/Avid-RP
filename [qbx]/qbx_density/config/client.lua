8fea4f26ca626fb33cc4440d0bb8c62a28a0b49d
njtank <106280943+njtank@users.noreply.github.com>
2024-11-07 18:07:59 -0500

    Decreasing density of population

--- [qbx]/qbx_density/config/client.lua
+++ [qbx]/qbx_density/config/client.lua
@@ -3,9 +3,9 @@ return {
     -- Minimum value is 0.0, maximum value is 1.0
     -- Value of 1.0 represents GTA Online populate rates
     -- Value of 0.0 removes all of that type
-    parked = 0.8, -- Density of parked vehicles
-    vehicle = 0.8, -- Density of vehicles
-    randomvehicles = 0.8, -- Density of random vehicles
-    peds = 0.8, -- Density of random peds (civilians, etc.)
-    scenario = 0.8, -- Density of scenario peds (bikers, gangsters, etc.)
+    parked = 0.6, -- Density of parked vehicles
+    vehicle = 0.5, -- Density of vehicles
+    randomvehicles = 0.5, -- Density of random vehicles
+    peds = 0.6, -- Density of random peds (civilians, etc.)
+    scenario = 0.5, -- Density of scenario peds (bikers, gangsters, etc.)
 }

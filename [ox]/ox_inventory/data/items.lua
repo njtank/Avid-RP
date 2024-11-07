84864afe8f2a313081d0a001ed9396cd025cb539
njtank <106280943+njtank@users.noreply.github.com>
2024-11-07 17:02:29 -0500

    Update items.lua

--- [ox]/ox_inventory/data/items.lua
+++ [ox]/ox_inventory/data/items.lua
@@ -57,8 +57,8 @@ return {
         label = 'Bandage',
         weight = 115,
         client = {
-            anim = { dict = 'missheistdockssetup1clipboard@idle_a', clip = 'idle_a', flag = 49 },
-            prop = { model = `prop_rolled_sock_02`, pos = vec3(-0.14, -0.14, -0.08), rot = vec3(-50.0, -50.0, 0.0) },
+            anim = { dict = 'anim@mp_player_intuppersalsa_roll', clip = 'idle_a', flag = 49 },
+            --prop = { model = `prop_rolled_sock_02`, pos = vec3(-0.14, -0.14, -0.08), rot = vec3(-50.0, -50.0, 0.0) },
             disable = { move = false, car = false, combat = true },
             usetime = 2500,
         }

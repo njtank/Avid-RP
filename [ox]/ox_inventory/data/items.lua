f01417f112cde40339d48c2b168394a2682f5a08
njtank <106280943+njtank@users.noreply.github.com>
2024-11-07 16:06:45 -0500

    Updating bandage animation

--- [ox]/ox_inventory/data/items.lua
+++ [ox]/ox_inventory/data/items.lua
@@ -59,7 +59,7 @@ return {
         client = {
             anim = { dict = 'missheistdockssetup1clipboard@idle_a', clip = 'idle_a', flag = 49 },
             prop = { model = `prop_rolled_sock_02`, pos = vec3(-0.14, -0.14, -0.08), rot = vec3(-50.0, -50.0, 0.0) },
-            disable = { move = true, car = true, combat = true },
+            disable = { move = false, car = false, combat = true },
             usetime = 2500,
         }
     },

9bc70c09b3e61ad1d3285d89b0b775a8c6b5f94b
njtank <106280943+njtank@users.noreply.github.com>
2024-11-10 10:12:48 -0500

    Adding package drop locations

--- [crime]/avid-parceltheft/config.lua
+++ [crime]/avid-parceltheft/config.lua
@@ -49,161 +49,346 @@ Config.Locations = {
         coords = vector3(1060.63, -378.30, 67.24),
         heading = 50.0,
         distance = 25.0,
-        prop = 'xm3_prop_xm3_product_box_01',
+        prop = 'prop_cs_box_clothes',
         debug = false
     },
     {
         coords = vector3(1010.23, -423.59, 64.35),
         heading = -52.00,
         distance = 25.0,
-        prop = 'xm3_prop_xm3_product_box_01',
+        prop = 'prop_cs_box_clothes',
         debug = false
     },
     {
         coords = vector3(1028.81, -409.67, 64.95),
         heading = -50.00,
         distance = 25.0,
-        prop = 'xm3_prop_xm3_product_box_01',
+        prop = 'prop_cs_box_clothes',
         debug = false
     },
     {
         coords = vector3(1056.19, -449.07, 65.26),
         heading = -10.00,
         distance = 25.0,
-        prop = 'xm3_prop_xm3_product_box_01',
+        prop = 'prop_cs_box_clothes',
         debug = false
     },
     {
         coords = vector3(893.20, -540.62, 57.51),
         heading = -63.00,
         distance = 25.0,
-        prop = 'xm3_prop_xm3_product_box_01',
+        prop = 'prop_cs_box_clothes',
         debug = false
     },
     {
         coords = vector3(850.28, -532.66, 56.93),
         heading = -94.00,
         distance = 25.0,
-        prop = 'xm3_prop_xm3_product_box_01',
+        prop = 'prop_cs_box_clothes',
         debug = false
     },
     {
         coords = vector3(861.73, -583.54, 57.16),
         heading = 2.00,
         distance = 25.0,
-        prop = 'xm3_prop_xm3_product_box_01',
+        prop = 'prop_cs_box_clothes',
         debug = false
     },
     {
         coords = vector3(980.31, -627.75, 58.24),
         heading = 37.00,
         distance = 25.0,
-        prop = 'xm3_prop_xm3_product_box_01',
+        prop = 'prop_cs_box_clothes',
         debug = false
     },
     {
         coords = vector3(959.95, -669.93, 57.45),
         heading = -61.00,
         distance = 25.0,
-        prop = 'xm3_prop_xm3_product_box_01',
+        prop = 'prop_cs_box_clothes',
         debug = false
     },
     {
         coords = vector3(996.79, -729.64, 56.82),
         heading = -50.00,
         distance = 25.0,
-        prop = 'xm3_prop_xm3_product_box_01',
+        prop = 'prop_cs_box_clothes',
         debug = false
     },
     {
         coords = vector3(1201.07, -575.49, 68.14),
         heading = -46.00,
         distance = 25.0,
-        prop = 'xm3_prop_xm3_product_box_01',
+        prop = 'prop_cs_box_clothes',
         debug = false
     },
     {
         coords = vector3(1265.15, -704.05, 63.54),
         heading = -30.00,
         distance = 25.0,
-        prop = 'xm3_prop_xm3_product_box_01',
+        prop = 'prop_cs_box_clothes',
         debug = false
     },
     {
         coords = vector3(1303.17, -527.39, 70.47),
         heading = -20.00,
         distance = 25.0,
-        prop = 'xm3_prop_xm3_product_box_01',
+        prop = 'prop_cs_box_clothes',
         debug = false
     },
     {
         coords = vector3(1373.14, -555.82, 73.69),
         heading = 70.00,
         distance = 25.0,
-        prop = 'xm3_prop_xm3_product_box_01',
+        prop = 'prop_cs_box_clothes',
         debug = false
     },
     {
         coords = vector3(1341.33, -597.35, 73.71),
         heading = 55.00,
         distance = 25.0,
-        prop = 'xm3_prop_xm3_product_box_01',
+        prop = 'prop_cs_box_clothes',
         debug = false
     },
     {
         coords = vector3(1301.04, -574.35, 70.74),
         heading = -13.00,
         distance = 25.0,
-        prop = 'xm3_prop_xm3_product_box_01',
+        prop = 'prop_cs_box_clothes',
         debug = false
     },
     {
         coords = vector3(1250.80, -515.48, 68.35),
         heading = -105.00,
         distance = 25.0,
-        prop = 'xm3_prop_xm3_product_box_01',
+        prop = 'prop_cs_box_clothes',
         debug = false
     },
     {
         coords = vector3(1262.60, -429.84, 69.02),
         heading = -66.00,
         distance = 25.0,
-        prop = 'xm3_prop_xm3_product_box_01',
+        prop = 'prop_cs_box_clothes',
         debug = false
     },
     {
         coords = vector3(1100.93, -411.39, 66.56),
         heading = -100.00,
         distance = 25.0,
-        prop = 'xm3_prop_xm3_product_box_01',
+        prop = 'prop_cs_box_clothes',
         debug = false
     },
     {
         coords = vector3(1009.71, -572.51, 59.60),
         heading = -99.00,
         distance = 25.0,
-        prop = 'xm3_prop_xm3_product_box_01',
+        prop = 'prop_cs_box_clothes',
         debug = false
     },
     {
         coords = vector3(965.08, -543.30, 58.36),
         heading = -60.00,
         distance = 25.0,
-        prop = 'xm3_prop_xm3_product_box_01',
+        prop = 'prop_cs_box_clothes',
         debug = false
     },
     {
         coords = vector3(1045.53, -497.57, 63.08),
         heading = -103.00,
         distance = 25.0,
-        prop = 'xm3_prop_xm3_product_box_01',
+        prop = 'prop_cs_box_clothes',
         debug = false
     },
     {
         coords = vector3(993.62, -620.83, 58.05),
         heading = -57.00,
         distance = 25.0,
-        prop = 'xm3_prop_xm3_product_box_01',
+        prop = 'prop_cs_box_clothes',
         debug = false
-    }
+    },
+    { --- ADDED ON FOR AVID RP
+        coords = vec3(928.67, -639.41, 58.24),
+        heading = -57.00,
+        distance = 25.0,
+        prop = 'prop_cs_box_clothes',
+        debug = false
+    },
+    { 
+        coords = vec3(902.87, -615.89, 58.45),
+        heading = -57.00,
+        distance = 25.0,
+        prop = 'prop_cs_box_clothes',
+        debug = false
+    },
+    { 
+        coords = vec3(886.58, -608.03, 58.45),
+        heading = -57.00,
+        distance = 25.0,
+        prop = 'prop_cs_box_clothes',
+        debug = false
+    },
+    { 
+        coords = vec3(844.15, -563.11, 57.83),
+        heading = -57.00,
+        distance = 25.0,
+        prop = 'prop_cs_box_clothes',
+        debug = false
+    },
+    { 
+        coords = vec3(920.26, -569.9, 58.37),
+        heading = -57.00,
+        distance = 25.0,
+        prop = 'prop_cs_box_clothes',
+        debug = false
+    },
+    { 
+        coords = vec3(963.98, -596.67, 59.9),
+        heading = -57.00,
+        distance = 25.0,
+        prop = 'prop_cs_box_clothes',
+        debug = false
+    },
+    { 
+        coords = vec3(977.03, -580.33, 59.85),
+        heading = -57.00,
+        distance = 25.0,
+        prop = 'prop_cs_box_clothes',
+        debug = false
+    },
+    { 
+        coords = vec3(1005.9, -510.79, 60.83),
+        heading = -57.00,
+        distance = 25.0,
+        prop = 'prop_cs_box_clothes',
+        debug = false
+    },
+    { 
+        coords = vec3(1207.04, -619.99, 66.44),
+        heading = -57.00,
+        distance = 25.0,
+        prop = 'prop_cs_box_clothes',
+        debug = false
+    },
+    --- END OF MIRROR PARK
+    --- START APARTMENTS IN VINEWOOD 
+    { 
+        coords = vec3(-314.49, 84.04, 67.58),
+        heading = -57.00,
+        distance = 25.0,
+        prop = 'prop_cs_box_clothes',
+        debug = false
+    },
+    { 
+        coords = vec3(-304.14, 80.38, 72.67),
+        heading = -57.00,
+        distance = 25.0,
+        prop = 'prop_cs_box_clothes',
+        debug = false
+    },
+    { 
+        coords = vec3(-302.94, 84.61, 76.66),
+        heading = -57.00,
+        distance = 25.0,
+        prop = 'prop_cs_box_clothes',
+        debug = false
+    },
+    { 
+        coords = vec3(-321.44, 84.3, 75.79),
+        heading = -57.00,
+        distance = 25.0,
+        prop = 'prop_cs_box_clothes',
+        debug = false
+    },
+    { 
+        coords = vec3(-313.98, 84.33, 71.66),
+        heading = -57.00,
+        distance = 25.0,
+        prop = 'prop_cs_box_clothes',
+        debug = false
+    },
+    { 
+        coords = vec3(-431.74, 83.67, 68.51),
+        heading = -57.00,
+        distance = 25.0,
+        prop = 'prop_cs_box_clothes',
+        debug = false
+    },
+    { 
+        coords = vec3(-431.66, 83.7, 72.52),
+        heading = -57.00,
+        distance = 25.0,
+        prop = 'prop_cs_box_clothes',
+        debug = false
+    },
+    { 
+        coords = vec3(-452.55, 78.64, 63.16),
+        heading = -57.00,
+        distance = 25.0,
+        prop = 'prop_cs_box_clothes',
+        debug = false
+    },
+    { 
+        coords = vec3(-452.58, 78.6, 71.34),
+        heading = -57.00,
+        distance = 25.0,
+        prop = 'prop_cs_box_clothes',
+        debug = false
+    },
+    { 
+        coords = vec3(-410.66, 160.23, 69.72),
+        heading = -57.00,
+        distance = 25.0,
+        prop = 'prop_cs_box_clothes',
+        debug = false
+    },
+    { 
+        coords = vec3(-412.12, 152.19, 73.74),
+        heading = -57.00,
+        distance = 25.0,
+        prop = 'prop_cs_box_clothes',
+        debug = false
+    },
+    { 
+        coords = vec3(-385.02, 159.6, 73.73),
+        heading = -57.00,
+        distance = 25.0,
+        prop = 'prop_cs_box_clothes',
+        debug = false
+    },
+    { 
+        coords = vec3(-383.78, 152.89, 77.74),
+        heading = -57.00,
+        distance = 25.0,
+        prop = 'prop_cs_box_clothes',
+        debug = false
+    },
+    { 
+        coords = vec3(-383.92, 153.16, 81.74),
+        heading = -57.00,
+        distance = 25.0,
+        prop = 'prop_cs_box_clothes',
+        debug = false
+    },
+    { 
+        coords = vec3(-160.74, 160.9, 85.71),
+        heading = -57.00,
+        distance = 25.0,
+        prop = 'prop_cs_box_clothes',
+        debug = false
+    },
+    { 
+        coords = vec3(-160.71, 160.98, 81.7),
+        heading = -57.00,
+        distance = 25.0,
+        prop = 'prop_cs_box_clothes',
+        debug = false
+    },
+    { 
+        coords = vec3(20.18, 114.47, 83.27),
+        heading = -57.00,
+        distance = 25.0,
+        prop = 'prop_cs_box_clothes',
+        debug = false
+    },
+    --- END OF APARTMENTS IN VINEWOOD
 }

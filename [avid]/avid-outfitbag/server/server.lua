26bf0f377fdc53e396ae16309e7f17e8a818cc69
njtank <106280943+njtank@users.noreply.github.com>
2024-11-12 21:40:56 -0500

    Major fixes to UI, HUD updated with engine toggle

--- [avid]/avid-outfitbag/server/server.lua
+++
@@ -1,7 +0,0 @@
-
-local ox = exports.ox_inventory
-
-RegisterNetEvent('krs_outfitbag:addItems', function(source)
-    local player = source
-    ox:AddItem(player, 'outfitbag', 1)
-end)
--- [standalone]/SK-Hud/client.lua
+++ [standalone]/SK-Hud/client.lua
@@ -362,3 +362,26 @@ RegisterNetEvent('hud:client:ToggleShowSeatbelt', function()
     vehicleStats.beltOn = not vehicleStats.beltOn
     updateVehicleStats()
 end)
+
+    --engine toggle
+    RegisterCommand('engine', function(source, args, rawCommand)
+        local playerPed = PlayerPedId()
+        local vehicle = GetVehiclePedIsIn(playerPed, false)
+
+        if DoesEntityExist(vehicle) and not IsEntityDead(vehicle) then
+            local engineState = GetIsVehicleEngineRunning(vehicle)
+
+            if engineState then
+                -- Engine is running, turn it off
+                SetVehicleEngineOn(vehicle, false, false, true)
+                TriggerEvent('chatMessage', '^^3Vehicle engine turned off.^r')
+            else
+                -- Engine is off, turn it on
+                SetVehicleEngineOn(vehicle, true, false, true)
+                TriggerEvent('chatMessage', '^^3Vehicle engine turned on.^r')
+            end
+        else
+            TriggerEvent('chatMessage', '^1You must be inside a vehicle to use this command.^r')
+        end
+    end, false)
+    RegisterKeyMapping('engine', 'Toggle Engine', 'keyboard', 'G')

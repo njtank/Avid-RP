bf0defebcb6e6d750851efb345951d1520ead421
njtank <106280943+njtank@users.noreply.github.com>
2024-11-20 18:18:58 -0500

    Reconfiguring car sounds and audio, added avid-carsounds

---
+++ [vehicles]/avid-carsounds/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+
+game 'gta5'
+
+files {
+  'audioconfig/*.dat151.rel',
+  'audioconfig/*.dat54.rel',
+  'sfx/**/*.awc'
+}
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/*.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/*.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/**/*.awc'

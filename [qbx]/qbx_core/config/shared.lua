6861ea36e406a2285f8c57ac7a1ab7fa4fcdfa93
njtank <106280943+njtank@users.noreply.github.com>
2024-11-21 16:04:59 -0500

    Future Justin, DONT FUCKING TOUCH THIS!

--- [qbx]/qbx_core/config/shared.lua
+++ [qbx]/qbx_core/config/shared.lua
@@ -1,5 +1,5 @@
 return {
-    serverName = 'Avid RP',
+    serverName = 'Server',
     defaultSpawn = vec4(-540.58, -212.02, 37.65, 208.88),
     notifyPosition = 'top-right', -- 'top' | 'top-right' | 'top-left' | 'bottom' | 'bottom-right' | 'bottom-left'
     ---@type { name: string, amount: integer, metadata: fun(source: number): table }[]

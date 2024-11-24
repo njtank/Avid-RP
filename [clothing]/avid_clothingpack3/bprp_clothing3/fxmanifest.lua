f39979a58b6d29ce01a6de63447ecdb5e154e121
njtank <106280943+njtank@users.noreply.github.com>
2024-11-24 10:49:34 -0500

    Adding clothing packs

---
+++ [clothing]/avid_clothingpack3/bprp_clothing3/fxmanifest.lua
@@ -0,0 +1,18 @@
+-- Generated with DurtyClothTool
+
+fx_version 'cerulean'
+game { 'gta5' }
+
+files {
+  'mp_m_freemode_01_mp_m_bprp_collection3.meta',
+  'mp_f_freemode_01_mp_f_bprp_collection3.meta',
+  'stream/pedalternatevariations.meta',
+  'stream/bprp_clothing3_overlays.xml',
+  'stream/shop_tattoo.meta'
+}
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_bprp_collection3.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_mp_f_bprp_collection3.meta'
+data_file 'ALTERNATE_VARIATIONS_FILE' 'stream/pedalternatevariations.meta'
+data_file 'PED_OVERLAY_FILE' 'stream/bprp_clothing3_overlays.xml'
+data_file 'TATTOO_SHOP_DLC_FILE' 'stream/shop_tattoo.meta'

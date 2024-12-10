c6de98b4d284a97537bebf984ddcc2db134a2b94
njtank <106280943+njtank@users.noreply.github.com>
2024-12-09 20:46:46 -0500

    Adding avid-zoom, for scroll wheel zoom in

---
+++ [avid]/avid-zoom/README.md
@@ -0,0 +1,5 @@
+# GFT_focus
+
+## This script will allow for players to be able to focus on a point
+
+## Simply drag and drop into your [qbx] (Rename folder to qbox_focus) If using QBCore simply add this to your [qb] and rename qb-focus
---
+++ [avid]/avid-zoom/client.lua
@@ -0,0 +1,54 @@
+local zoomed = false
+local zoomFOV = 30.0
+local normalFOV = 50.0
+local zoomSpeed = 2.0
+local camera = nil
+
+function interpolateFOV(currentFOV, targetFOV, speed)
+    return currentFOV + (targetFOV - currentFOV) / speed
+end
+
+RegisterKeyMapping("holdZoom", "Hold to Zoom", "keyboard", "SCROLLWHEEL_BUTTON")
+
+CreateThread(function()
+    while true do
+        Wait(0)
+        local isZoomKeyHeld = IsControlPressed(0, 27) -- Scroll wheel press (Control ID 27)  
+        local targetFOV
+
+        if isZoomKeyHeld then
+            if not zoomed then
+                zoomed = true
+                if not camera then
+                    camera = CreateCam("DEFAULT_SCRIPTED_CAMERA", true)
+                    SetCamActive(camera, true)
+                    RenderScriptCams(true, false, 0, true, true)
+                end
+                SetCamCoord(camera, GetGameplayCamCoord())
+                SetCamRot(camera, GetGameplayCamRot(2), 2)
+            end
+            targetFOV = zoomFOV
+        else
+            if zoomed then
+                zoomed = false
+            end
+            targetFOV = normalFOV
+        end
+
+        if camera then
+            local currentFOV = GetCamFov(camera)
+            local newFOV = interpolateFOV(currentFOV, targetFOV, zoomSpeed)
+            SetCamFov(camera, newFOV)
+            local gameplayCamCoords = GetGameplayCamCoord()
+            local gameplayCamRot = GetGameplayCamRot(2)
+            SetCamCoord(camera, gameplayCamCoords)
+            SetCamRot(camera, gameplayCamRot, 2)
+
+            if not zoomed and math.abs(newFOV - normalFOV) < 1.0 then
+                RenderScriptCams(false, false, 0, true, true)
+                DestroyCam(camera, false)
+                camera = nil
+            end
+        end
+    end
+end)

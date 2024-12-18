db0a62c1f227abb3df18b43db95aeff1c651e1a8
njtank <106280943+njtank@users.noreply.github.com>
2024-12-18 14:58:41 -0500

    Casino being removed for a re-work, be back soon

--- [civilian]/[casino]/DLCiplLoader/fxmanifest.lua
+++
@@ -1,8 +0,0 @@
-fx_version 'cerulean'
-games { "gta5" }
-lua54 'on'
-description "Fleecabank-rework bought from CikanaModding's tebex"
-client_script 'client.lua'
-
-version "1.0.0"
-this_is_a_map "yes"

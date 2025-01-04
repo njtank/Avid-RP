59257d5e1e8ee917ea239e833ee4104ec6339b36
njtank <106280943+njtank@users.noreply.github.com>
2025-01-04 10:46:36 -0500

    Adding avid-lsfd

---
+++ [MLO]/avid-lsfd/fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'bodacious'
+game 'gta5'
+
+author 'BlightyMods'
+description 'BGFS'
+version '1.0.0'
+
+this_is_a_map 'yes'
+
+
+
+files {
+	'interiorproxies.meta'
+}
+
+client_script {
+
+}
+ 
+  

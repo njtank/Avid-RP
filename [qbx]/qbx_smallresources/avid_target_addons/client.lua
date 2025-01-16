31dcfcd0b09b7c799859cacf3e028edc8d325037
njtank <106280943+njtank@users.noreply.github.com>
2025-01-16 09:18:05 -0500

    Adding target locking for vehicles

--- [qbx]/qbx_smallresources/avid_target_addons/client.lua
+++ [qbx]/qbx_smallresources/avid_target_addons/client.lua
@@ -53,3 +53,47 @@ exports.ox_target:addGlobalVehicle({
 RegisterNetEvent('police:client:ToggleEscort', function(toggle)
     isEscorted = toggle
 end)
+
+exports.ox_target.addGlobalVehicle(options, {
+    {
+        name = 'lockVehicle',
+        icon = 'fas fa-lock',
+        label = '🔑 Toggle Lock',
+        onSelect = function(data)
+            local vehicle = data.entity
+            if vehicle and DoesEntityExist(vehicle) then
+                local plate = GetVehicleNumberPlateText(vehicle)
+                -- Checks if the player has the key
+                if exports['Renewed-Vehiclekeys']:hasKey(plate) then
+                    -- Checks current vehicle status (locked or unlocked)
+                    local isLocked = Entity(vehicle).state.vehicleLock and Entity(vehicle).state.vehicleLock.lock == 2
+                    -- Changes vehicle status (locked/unlocked)
+                    Entity(vehicle).state:set('vehicleLock', {
+                        lock = isLocked and 1 or 2, -- 1 for unlocked, 2 for locked
+                        sound = true, -- Play a sound
+                    }, true)
+                    -- Send a success notification
+                    lib.notify({
+                        title = 'Notification',
+                        description = isLocked and 'Vehicle unlocked' or 'Vehicle locked',
+                        type = 'success',
+                        duration = 5000
+                    })
+                else
+                    lib.notify({
+                        title = 'Notification',
+                        description = 'You do not have the key to this vehicle',
+                        type = 'error',
+                        duration = 5000
+                    })
+                end
+            end
+        end,
+        canInteract = function(entity, distance, coords, name)
+            local plate = GetVehicleNumberPlateText(entity)
+            -- Allows interaction if the player has the key and the distance is less than 2.0
+            return exports['Renewed-Vehiclekeys']:hasKey(plate) and distance < 2.0
+        end
+    }
+})
+

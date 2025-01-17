d29a12fc7fff3396e6dede1bb8052f4e56b9264b
njtank <106280943+njtank@users.noreply.github.com>
2025-01-17 01:09:31 -0500

    Adding engine toggle to avid_hud

--- [avid]/avid_hud/client/Main.lua
+++ [avid]/avid_hud/client/Main.lua
@@ -92,3 +92,30 @@ RegisterCommand("hud", function()
       action = "openSettings"
    })
 end)
+
+local button = 56 -- 167 (F6 by default)
+local commandEnabled = false -- (false by default) If you set this to true, typing "/engine" in chat will also toggle your engine.
+
+Citizen.CreateThread(function()
+    if commandEnabled then
+        RegisterCommand('engine', function() 
+            toggleEngine()
+        end, false)
+    end
+    while true do
+        Citizen.Wait(0)
+        local vehicle = GetVehiclePedIsIn(PlayerPedId(), false)
+        
+        if (IsControlJustReleased(0, button) or IsDisabledControlJustReleased(0, button)) and vehicle ~= nil and vehicle ~= 0 and GetPedInVehicleSeat(vehicle, 0) then
+            toggleEngine()
+        end
+        
+    end
+end)
+
+function toggleEngine()
+    local vehicle = GetVehiclePedIsIn(PlayerPedId(), false)
+    if vehicle ~= nil and vehicle ~= 0 and GetPedInVehicleSeat(vehicle, 0) then
+        SetVehicleEngineOn(vehicle, (not GetIsVehicleEngineRunning(vehicle)), false, true)
+    end
+end

3d6f2fa958db8ef139fc1b2e7bf469e88bcb0afa
njtank <106280943+njtank@users.noreply.github.com>
2025-01-18 13:33:09 -0500

    Final configuration for avid-newspaper

--- [civilian]/avid-newspaper/shared.lua
+++ [civilian]/avid-newspaper/shared.lua
@@ -3,5 +3,5 @@ return {
     Ped = 'a_m_m_paparazzi_01',
     PedCoords = vec4(-604.82, -926.45, 22.86, 181.1),
     BikeModel = 'scorcher', -- Bicycle might be too slow, maybe a faggio? (scooter)
-    BikeSpawn = vec4(-602.74, -927.65, 23.86, 179.5),
+    BikeSpawn = vec4(-606.94, -928.28, 23.86, 178.52),
 }
